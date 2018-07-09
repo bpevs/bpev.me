02d5f28380cd98048eecd61b4ce8a54da3831284
ivebencrazy <ivebencrazy@users.noreply.github.com>
2018-07-08 19:39:29 -0700

    Update for Heroku

--- server.js
+++ server.js
@@ -1,34 +1,63 @@
 const express = require("express");
 const next = require("next");
+const path = require("path");
+const url = require("url");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
 
+// Multi-process to utilize all CPU cores.
+if (!dev && cluster.isMaster) {
+  console.log(`Node cluster master ${process.pid} is running`);
 
-app.prepare()
-  .then(() => {
-    const server = express();
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    server.use("/assets", express.static("assets"));
-
-    server.get("/post/:id", (req, res) => {
-      const actualPage = "/post";
-      const queryParams = { id: req.params.id };
-      app.render(req, res, actualPage, queryParams);
-    });
-
-    server.get("*", (req, res) => {
-      return handle(req, res);
-    });
-
-
-    server.listen(3000, (err) => {
-      if (err) throw err
-      console.log("> Ready on localhost:3000");
-    });
-  })
-  .catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
   });
+
+} else {
+  const nextApp = next({ dir: ".", dev });
+  const nextHandler = nextApp.getRequestHandler();
+
+  nextApp.prepare()
+    .then(() => {
+      const server = express();
+
+      if (!dev) {
+        // Enforce SSL & HSTS in production
+        server.use(function(req, res, next) {
+          var proto = req.headers["x-forwarded-proto"];
+          if (proto === "https") {
+            res.set({
+              "Strict-Transport-Security": "max-age=31557600" // one-year
+            });
+            return next();
+          }
+          res.redirect("https://" + req.headers.host + req.url);
+        });
+      }
+
+      server.use("/assets", express.static("assets"));
+
+      server.get("/post/:id", (req, res) => {
+        const actualPage = "/post";
+        const queryParams = { id: req.params.id };
+        nextApp.render(req, res, actualPage, queryParams);
+      });
+
+      server.get("*", (req, res) => {
+        return nextHandler(req, res);
+      });
+
+      server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`Listening on port ${port}`);
+      });
+    });
+}
