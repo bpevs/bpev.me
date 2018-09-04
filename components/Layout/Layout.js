dcf4d59474b1fabddb65776b7cd7cc1a0049f247
ivebencrazy <ivebencrazy@users.noreply.github.com>
2018-09-03 19:37:55 -0700

    Shitty impl of loader
    - but it works

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,24 +1,36 @@
 import Head from "next/head";
 import Header from "../LayoutHeader/LayoutHeader";
 import stylesheet from "../index.css";
+import NProgress from "nprogress"
+import Router from "next/router"
+import Link from "next/link";
 
+const LayoutHeader = (props) => {
+  if (Router.router) {
+    Router.router.events.on("routeChangeStart", (url) => {
+      NProgress.start()
+    })
+    Router.router.events.on("routeChangeComplete", () => NProgress.done())
+    Router.router.events.on("routeChangeError", () => NProgress.done())
+  }
 
-const LayoutHeader = (props) => (
-  <div>
-    <Head>
-      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-      <noscript>
-        <style dangerouslySetInnerHTML={{ __html: ".jsonly { display: none }" }} />
-      </noscript>
-    </Head>
+  return (
+    <div>
+      <Head>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+        <noscript>
+          <style dangerouslySetInnerHTML={{ __html: ".jsonly { display: none }" }} />
+        </noscript>
+      </Head>
 
-    <div className={"header sans-serif container m2 block mx-auto " + (props.className || "")}>
-      <Header />
-      <section className="clearfix mx-auto">
-        {props.children}
-      </section>
+      <div className={"header sans-serif container m2 block mx-auto " + (props.className || "")}>
+        <Header />
+        <section className="clearfix mx-auto">
+          {props.children}
+        </section>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default LayoutHeader;
