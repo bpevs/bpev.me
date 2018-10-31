92143f0100f48ac6b1cceda02a44c1d0523830f0
ivebencrazy <ivebencrazy@users.noreply.github.com>
2018-10-30 22:42:33 -0700

    Refactor server stuff

--- server.js
+++ server.js
@@ -1,54 +1,47 @@
-const express = require("express");
-const next = require("next");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
+const express = require("express")
+const next = require("next")
+const cluster = require("cluster")
+const numCPUs = require("os").cpus().length
 
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
-const host = dev ? "localhost" : "0.0.0.0";
+const dev = process.env.NODE_ENV !== "production"
+const port = Number(process.env.PORT) || 3000
+const host = dev ? "localhost" : "0.0.0.0"
 
 // Multi-process to utilize all CPU cores.
 if (!dev && cluster.isMaster) {
-  console.log(`Node cluster master ${process.pid} is running`);
+  console.log(`Node cluster master ${process.pid} is running`)
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    cluster.fork()
   }
 
   cluster.on("exit", (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`)
+  })
 
 } else {
-  const nextApp = next({ dir: ".", dev });
-  const nextHandler = nextApp.getRequestHandler();
+  const nextApp = next({ dir: ".", dev })
+  const nextHandler = nextApp.getRequestHandler()
 
   nextApp.prepare()
     .then(() => {
-      const server = express();
+      const server = express()
 
-      server.use("/static", express.static("static"));
+      server.use("/static", express.static("static"))
 
-      server.get("/:postId", (req, res) => {
-        nextApp.render(req, res, "/post", req.params);
-      });
+      server.get("/:postId", renderRoute("/post"))
+      server.get("/post/:postId", renderRoute("/post"))
+      server.get("/:postId/:mediaId", renderRoute("/media"))
+      server.get("*", (req, res) => nextHandler(req, res))
 
-      server.get("/post/:postId", (req, res) => {
-        nextApp.render(req, res, "/post", req.params);
-      });
-
-      server.get("/:postId/:mediaId", (req, res) => {
-        nextApp.render(req, res, "/media", req.params);
+      server.listen(port, host, (err) => {
+        if (err) throw err
+        console.log(`Listening on port ${port}`)
       })
+    })
 
-      server.get("*", (req, res) => {
-        return nextHandler(req, res);
-      });
-
-      server.listen(host, port, (err) => {
-        if (err) throw err;
-        console.log(`Listening on port ${port}`);
-      });
-    });
+  function renderRoute(route) {
+    return (req, res) => nextApp.render(req, res, route, req.params)
+  }
 }
