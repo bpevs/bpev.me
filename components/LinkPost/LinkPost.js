95824677b93a0c55f23e427b4d7cdc7427e0dd0e
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2018-11-01 18:28:25 -0700

    Pretty-print dates (#45)

--- components/LinkPost/LinkPost.js
+++ components/LinkPost/LinkPost.js
@@ -2,7 +2,28 @@ import Link from "next/link"
 import React from "react"
 import Tag from "../Tag/Tag"
 
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
+
 export default function LinkPost({ post }) {
+  let date = ""
+  if (post.createdDate) {
+    const [ y, m, d ] = post.createdDate.split("-")
+    date = `${months[Number(m) - 1]} ${d}, ${y}`
+  }
+
   return (
     <Link
       as={`/${post.id}`}
@@ -12,7 +33,7 @@ export default function LinkPost({ post }) {
       <li key={post.id} className="p1 m1 mt3 link-post">
           <span className="h3 link-post-title">{post.title}</span>
           <div>
-            <span className="h5 black pt2">{post.createdDate} | {" "}</span>
+            <span className="h5 black pt2">{date}</span>
             <span>{post.tags.map((tag, index) => <Tag key={index}>{tag}</Tag>)}</span>
           </div>
       </li>
