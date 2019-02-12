11a0b073abbfb59cdf5078e32d8c93a121a7d3c1
ivebencrazy <ivebencrazy@users.noreply.github.com>
2019-02-12 05:53:15 -0800

    Remove firebase

--- constants.js
+++ constants.js
@@ -6,13 +6,3 @@ export const CONTENT_TYPE = {
   ARTICLE: "article",
   GALLERY: "gallery",
 }
-
-// This is the public Firebase configuration. These keys are simply used for
-// telling Firebase where our apis are located, and are not a security issue.
-// Our Firebase security rules are what prevent shenanigans.
-export const FIREBASE = {
-  apiKey: "AIzaSyDVl8Tdzu8WdyqSoW7fd-FHujItUxTGW1M",
-  authDomain: "open-acappella.firebaseapp.com",
-  databaseURL: "https://open-acappella.firebaseio.com/",
-  storageBucket: "gs://open-acappella.appspot.com",
-}
