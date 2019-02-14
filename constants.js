737a991d934e69c25c1fdf19d507b714f0668b0b
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2019-02-14 05:59:32 -0800

    近藤 麻理恵 this shit (#63)
    Clean up repo, remove things that don't spark joy

--- constants.js
+++
@@ -1,8 +0,0 @@
-import { get } from "@civility/utilities"
-
-export const ASSET_URL = get(process, [ "env", "ASSET_URL" ]) || "https://static.bpev.me"
-
-export const CONTENT_TYPE = {
-  ARTICLE: "article",
-  GALLERY: "gallery",
-}
