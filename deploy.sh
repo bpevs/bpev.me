762c4fabb19e611c443123aeaacffe212e7c9356
ivebencrazy <github@bpev.me>
2022-02-09 16:03:02 -0800

    fix: add import-map to script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.18.0
 
 # download blog files
 echo "[ bpev ] Deploying Site..."
-/opt/buildhome/.deno/bin/deno run --allow-write --allow-net --allow-read --allow-env --unstable ./src/main.tsx b2 dist
+/opt/buildhome/.deno/bin/deno run --allow-write --allow-net --allow-read --allow-env --unstable --import-map ./import_map.json ./src/main.tsx b2 dist
