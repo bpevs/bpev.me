27acc41597db904f60495ef2e2d7c85b8ae5c16b
ivebencrazy <git@bpev.me>
2022-05-24 13:47:50 -0700

    fix: fix link url

--- apps/link/main.js
+++ apps/link/main.js
@@ -2,9 +2,12 @@
 
 import { parse } from "https://deno.land/std@0.140.0/encoding/yaml.ts";
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { join } from "https://deno.land/std@0.140.0/path/mod.ts";
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.140.0/path/mod.ts";
 
-const urls = parse(
-  await Deno.readTextFile(join(import.meta.url, "sites.yaml")),
-);
+const urlsFilepath = join(dirname(fromFileUrl(import.meta.url)), "sites.yaml");
+const urls = parse(await Deno.readTextFile(urlsFilepath));
 serve((req) => Response.redirect(urls[new URL(req.url).pathname] || urls[404]));
