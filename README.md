7f5299864b9d033f7e16dd205ad1e9f8ff3ee071
ivebencrazy <git@bpev.me>
2022-05-24 13:16:24 -0700

    fix: update to use correct filepath

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # bpev.me
 
-This is a repository for `*.bpev.me` microapps. The actual
-[`https://bpev.me`](https://bpev.me) is a [Mataroa](https://mataroa.blog) blog.
+This is a repository for `*.bpev.me` microapps. [`bpev.me`](https://bpev.me) is
+a [Mataroa](https://mataroa.blog) blog.
--- apps/video/main.js
+++ apps/video/main.js
@@ -1,9 +1,13 @@
 // deno run --allow-read --allow-net main.js
 
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { join } from "https://deno.land/std@0.140.0/path/mod.ts";
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.140.0/path/mod.ts";
 
-const index = join(import.meta.url, "index.html");
+const index = join(dirname(fromFileUrl(import.meta.url)), "index.html");
 
 serve(async () =>
   new Response((await Deno.open(index, { read: true })).readable)
