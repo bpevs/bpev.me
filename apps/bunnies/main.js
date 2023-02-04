b34d6bb71e9e9bf7e56b4ce64b406846b42891d8
Ben Pevsner <git@bpev.me>
2023-02-04 12:28:25 +0800

    fix(bunnies): update bunnies for deno-deploy

--- apps/bunnies/main.js
+++ apps/bunnies/main.js
@@ -1,7 +1,14 @@
 // deno run --allow-read --allow-net main.js
+
+// @deno-types="https://deno.land/x/esbuild@v0.14.51/mod.d.ts"
+import * as esbuildWasm from "esbuild/wasm.js";
+import * as esbuildNative from "esbuild/mod.js";
+
+// @ts-ignore
+const esbuild = Deno.run === undefined ? esbuildWasm : esbuildNative;
+
 import { serve } from '$std/http/server.ts'
 import { dirname, fromFileUrl, join, resolve } from '$std/path/mod.ts'
-import * as esbuild from 'esbuild';
 import { denoPlugin } from 'esbuild_deno_loader';
 let importMapURL = new URL('file://' + resolve('./import_map.json'));
 
