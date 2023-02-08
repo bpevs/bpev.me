d824fedc97a0e2fad0b5a556854e11c652eed099
Ben Pevsner <git@bpev.me>
2023-02-09 01:55:42 +0800

    fix: use fast image on smaller screen

--- islands/photo.tsx
+++ islands/photo.tsx
@@ -1,7 +1,6 @@
-import { parse } from '$std/path/mod.ts'
+import type { Note } from '@/utilities/notes.ts'
 import { URL_STATIC } from '@/constants.ts'
 import { SIZE, Size } from '@/utilities/image.ts'
-import { Note } from '@/utilities/notes.ts'
 
 interface Props {
   src: string
@@ -19,17 +18,35 @@ export default function Image({ src, size = 'normal', note }: Props) {
   }
   const normalizedSize = size.toUpperCase() || NORMAL
 
-  const { ext, name } = parse(src)
+  const [name, ext] = src.split('.')
   const originalPath = 'notes/' + note.slug + '/' + name + ext.toUpperCase()
   const webpPath = 'notes/' + note.slug + '/' + name + '.WEBP'
 
   return (
-    <a href={URLS.DETAILED + originalPath}>
+    <a
+      href={URLS.NORMAL + webpPath}
+      style={{
+        textAlign: 'center',
+        display: 'block',
+        width: '100%',
+        maxHeight: '800px',
+      }}
+    >
       <picture>
-        <source srcset={URLS[normalizedSize] + webpPath} type='image/webp' />
-        <img
-          type={`image/${ext.replace('.', '')}`}
+        <source
+          srcset={URLS[normalizedSize] + webpPath}
+          media='(min-width: 900px)'
+          type='image/webp'
+        />
+        <source srcset={URLS.FAST + webpPath} type='image/webp' />
+        <source
           srcset={URLS[normalizedSize] + originalPath}
+          media='(min-width: 900px)'
+          type={`image/${ext}`}
+        />
+        <source srcset={URLS.FAST + originalPath} type={`image/${ext}`} />
+        <img
+          type={`image/${ext}`}
           src={URLS.ORIGINAL + originalPath}
         />
       </picture>
