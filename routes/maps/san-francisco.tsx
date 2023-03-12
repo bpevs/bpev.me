fd0e09cf5b5a4872d8437cca707d9672413e4ee9
Ben Pevsner <git@bpev.me>
2023-03-12 12:19:08 +0000

    Create san-francisco.tsx

---
+++ routes/maps/san-francisco.tsx
@@ -0,0 +1,10 @@
+import Page from '@/components/page.tsx'
+import GeoMap from '@/islands/geomap.tsx'
+
+export default function SanFrancisco() {
+  return (
+    <Page hideFooter>
+      <GeoMap />
+    </Page>
+  )
+}
